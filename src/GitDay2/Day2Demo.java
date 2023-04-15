94946e0bdeda040905f269ce0c1f0eda2f508aaf
feb27 <feb27@AsusVane>
2023-04-14 23:17:23 -0400

    IMPLEMENTED divide()

--- src/GitDay2/Day2Demo.java
+++ src/GitDay2/Day2Demo.java
@@ -2,9 +2,16 @@ package GitDay2;
 
 public class Day2Demo {
 
-	public void getInfo() {
+	public void subtract(int a, int b) {
 		
-		System.out.println("Pushing code..");
+		System.out.println(a-b);
+		
+	}
+	
+	public void divide(int a, int b){
+		
+	System.out.println(a/b);
 	
 	}
-}
+	
+	}
